f80ecc14147281e756d986a265197dac8db47892
eden <mikkusumanga@gmail.com>
2024-02-24 12:56:50 -0400

    first commit

---
+++ README.md
@@ -0,0 +1,10 @@
+Schema from JSON CV:
+https://jsonresume.org/schema/
+
+<img src="screenshot.png"></img>
+
+Based on the design of:
+https://github.com/BartoszJarocki/cv and https://github.com/midudev/minimalist-portfolio-json
+
+
+Edit the `cv.json` file to create your own printable CV.
---
+++ src/i18n/ui.ts
@@ -0,0 +1,40 @@
+export const defaultLang = 'en';
+
+export const showDefaultLang = false;
+
+export const ui = {
+    en: {
+        about: 'About',
+        experience: 'Experience',
+        projects: 'Projects',
+        skills: 'Skills',
+        search: 'Search command',
+        press: 'Press',
+        forOpenPalette: 'for open command pallette',
+        actions: 'Actions',
+        social: 'Social',
+        visit: 'Visit',
+        toSelect: 'to select',
+        toNavigate: 'to navigate',
+        toClose: 'to close',
+        moveToParent: 'Move to parent',
+        print: 'Print',
+    },
+    es: {
+        about: 'Sobre mi',
+        experience: 'Experiencia',
+        projects: 'Proyectos',
+        skills: 'Habilidades',
+        search: 'Buscar comando',
+        press: 'Presiona',
+        forOpenPalette: 'para abrir el paleta de comandos',
+        actions: 'Acciones',
+        social: 'Social',
+        visit: 'Visitar',
+        toSelect: 'para seleccionar',
+        toNavigate: 'para navegar',
+        toClose: 'para cerrar',
+        moveToParent: 'Mover al padre',
+        print: 'Imprimir',
+    }
+} as const;
